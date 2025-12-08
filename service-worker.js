5a814456d3d587ff2dda46809a4c77114c70b99b
botvlad <g11410900@gmail.com>
2025-12-08 23:54:37 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,23 @@
-self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>self.clients.claim());
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('forex-pwa-cache').then((cache) => {
+            return cache.addAll([
+                '/icon-192.png',
+                '/icon-512.png',
+                '/',
+                '/index.html',
+                '/style.css',
+                '/app.js',
+                '/manifest.json'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
